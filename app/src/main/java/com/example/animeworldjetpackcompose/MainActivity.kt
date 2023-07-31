c2fa7e6819bff174adab31acc5137cb00043bea1
mvn-toannguyen2 <toan.nguyen2@monstar-lab.com>
2023-07-31 09:55:25 +0700

    fix comment ci:

--- app/src/main/java/com/example/animeworldjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/animeworldjetpackcompose/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
