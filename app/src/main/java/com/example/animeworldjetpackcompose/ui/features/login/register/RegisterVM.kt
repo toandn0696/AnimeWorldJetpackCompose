603537808cc6bd68df1221f92590a2a45a202158
mvn-toannguyen2 <toan.nguyen2@monstar-lab.com>
2023-08-07 09:59:35 +0700

    fix cmt ci

--- app/src/main/java/com/example/animeworldjetpackcompose/ui/features/login/register/RegisterVM.kt
+++ app/src/main/java/com/example/animeworldjetpackcompose/ui/features/login/register/RegisterVM.kt
@@ -1,7 +1,13 @@
 package com.example.animeworldjetpackcompose.ui.features.login.register
 
+import com.example.animeworldjetpackcompose.arch.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
 /**
  * @author mvn-toan.nguyen2 on 8/2/23
  **/
-class RegisterVM {
+
+@HiltViewModel
+class RegisterVM @Inject constructor() : BaseViewModel() {
 }
